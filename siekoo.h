71137ad1b8ba7226e3cd611c67ab991b0db541a6
Artem Kulchenko <64947477+ua2fga@users.noreply.github.com>
2024-01-04 22:16:15 +0200

    Add files via upload

---
+++ siekoo.h
@@ -0,0 +1,47 @@
+// SIEKOO ALPHABET FOR 7-SEGMENT DISPLAY
+// Care was taken to ensure that **no double meanings as a letter AND number** occur
+// https://fonts-online.ru/fonts/siekoo
+
+#ifndef __SIEKOO_H__
+#define __SIEKOO_H__
+//header file content
+
+#define _A 0b1011111
+#define _B 0b1111100
+#define _C 0b1011000
+#define _D 0b1011110
+#define _E 0b1111001
+#define _F 0b1110001
+#define _G 0b0111101 
+#define _H 0b1110100
+#define _I 0b0010001
+#define _J 0b0001101
+#define _K 0b1110101 
+#define _L 0b0111000 
+#define _M 0b1010101 
+#define _N 0b1010100
+#define _O 0b1011100
+#define _P 0b1110011
+#define _Q 0b1100111
+#define _R 0b1010000
+#define _S 0b0101101
+#define _T 0b1111000
+#define _U 0b0011100 
+#define _V 0b0101010
+#define _W 0b1101010
+#define _X 0b0010100
+#define _Y 0b1101110 
+#define _Z 0b0011011
+
+#define _0 0b0111111 
+#define _1 0b0000110 
+#define _2 0b1011011 
+#define _3 0b1001111 
+#define _4 0b1100110 
+#define _5 0b1101101 
+#define _6 0b1111101 
+#define _7 0b0000111 
+#define _8 0b1111111 
+#define _9 0b1101111
+
+#endif
